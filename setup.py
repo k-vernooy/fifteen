911d39d219ff9fc2c8612cab8bc7e130c8468694
k-vernooy <kai.a.vernooy@gmail.com>
2020-04-22 11:34:09 -0400

    Python structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fifteen",
+    author="Kai Vernooy",
+    description="A terminal version of the fifteen game",
+    author_email="kai.a.vernooy@gmail.com",
+    url="https://github.com/k-vernooy/fifteen",
+    package=find_packages()
+)
