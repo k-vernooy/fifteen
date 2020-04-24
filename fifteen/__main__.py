c69a3b992e86b8c0e1118716173bb1b3d02520e3
k-vernooy <kai.a.vernooy@gmail.com>
2020-04-24 11:50:18 -0400

    Add timer

--- fifteen/__main__.py
+++ fifteen/__main__.py
@@ -2,7 +2,8 @@ import board
 import sys
 import signal
 import curses
-
+from time import sleep, process_time
+from threading import Thread
 
 def signal_handler(sig, frame):
     curses.nocbreak()
@@ -10,21 +11,43 @@ def signal_handler(sig, frame):
     curses.endwin()
     sys.exit(0)
 
+
 def mainloop():
-    signal.signal(signal.SIGINT, signal_handler)
+    delay = 0.01
+    time = 0
     b = board.Board(4, 4)
+    b.draw()
 
     while not b.complete():
-        b.draw()
+        ch = b.stdscr.getch()
+        if ch == curses.KEY_UP:
+            b.move(1)
+        elif ch == curses.KEY_RIGHT:
+            b.move(2)
+        elif ch == curses.KEY_DOWN:
+            b.move(3)
+        elif ch == curses.KEY_LEFT:
+            b.move(4)
+
+        if ch != -1:
+            b.draw()
+
+        b.stdscr.addstr(14,0,str(round(time * 1000) / 1000))
+        sleep(delay)
+        time += delay
+
 
     b.stdscr.addstr(12,0,"\ngame over! press any key to exit, or r to restart")
-    ch = b.draw()
+    b.stdscr.nodelay(0)
+
+    ch = b.stdscr.getch()
     
     if ch == 114:
         mainloop()
 
     b.cleanup()
     sys.exit(0)
-    signal.pause()
 
+signal.signal(signal.SIGINT, signal_handler)
 mainloop()
+signal.pause()
